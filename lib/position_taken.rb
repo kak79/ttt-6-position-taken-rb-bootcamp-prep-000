d08c169288dc8d29e9b9a57178c072517a39042e
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-25 19:29:42 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 #Address the "edge cases" - scenarios that qualify as an empty space that isn't equal to the literal " ".
 #For example, we would want to consider "" to be an empty space as well.
 #If the position is free, the method should return false (i.e. "not taken"); 
-#
+#otherwise it will return true.
 
 def position_taken?()
 
