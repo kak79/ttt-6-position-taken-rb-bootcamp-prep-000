1c9e6138997500af60bb68fc21e054cf49b377cd
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-25 19:26:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 #Define a method that checks if a given space on the board is empty, i.e. equal to " ".
 #Address the "edge cases" - scenarios that qualify as an empty space that isn't equal to the literal " ".
-#
+#For example, we would want to consider "" to be an empty space as well.
 
 
 
